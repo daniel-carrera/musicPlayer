3e95aa5410aac382bf07bc1f162fa2746198b2fa
daniel-carrera <dcarre1@ucsc.edu>
2020-12-28 22:24:28 -0800

    first xcode files commit

---
+++ musicPlayerApp/musicPlayerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  musicPlayerApp
+//
+//  Created by Daniel Carrera on 12/28/20.
+//  Copyright © 2020 Daniel Carrera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
